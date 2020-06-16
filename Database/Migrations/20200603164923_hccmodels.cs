e630c7f85e5458e478a3f07cc589440237dad0fc
Jonasz Makulak <j.makulak@gmail.com>
2020-06-16 22:53:22 +0200

    Some changes

---
+++ Database/Migrations/20200603164923_hccmodels.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PotatoServer.Migrations
+{
+    public partial class hccmodels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AppearanceRating",
+                table: "Statistics",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ComfortRating",
+                table: "Statistics",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AppearanceRating",
+                table: "Statistics");
+
+            migrationBuilder.DropColumn(
+                name: "ComfortRating",
+                table: "Statistics");
+        }
+    }
+}
---
+++ Hubs/Rooms/RoomsHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PotatoServer.Hubs.Rooms
+{
+    public class RoomsHub : Hub
+    {
+        public async Task GetRooms(int? skip, int? take)
+        {
+            await Task.FromResult("");
+        }
+    }
+}
