121432e7ff3bb294d1d27064fb6ddcd342f51a11
Jonasz Makulak <j.makulak@gmail.com>
2020-06-23 19:08:52 +0200

    Some more cleaning

---
+++ IServiceCollectionExtensions.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.IdentityModel.Tokens;
+using PotatoServer.Database;
+using PotatoServer.Database.Models.Core;
+using PotatoServer.Exceptions;
+using PotatoServer.Filters;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PotatoServer
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection SetupCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(builder =>
+                {
+                    builder
+                    .WithOrigins("http://localhost:4200")
+                    .AllowCredentials()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+                });
+            });
+            return services;
+        }
+
+        public static IServiceCollection SetupMvc(this IServiceCollection services)
+        {
+            services.AddMvc(config =>
+            {
+                config.Filters.Add(new HandleExceptionAttribute());
+            })
+            .AddDataAnnotationsLocalization(options =>
+            {
+                options.DataAnnotationLocalizerProvider = (type, factory) => factory.Create(typeof(SharedResources));
+            });
+            return services;
+        }
+
+        public static IServiceCollection SetupIdentity(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddIdentity<User, IdentityRole>(o =>
+            {
+                try
+                {
+                    o.Password.RequiredLength = int.Parse(configuration["Password:RequiredLength"]);
+                    o.Password.RequireDigit = bool.Parse(configuration["Password:RequireDigit"]);
+                    o.Password.RequireLowercase = bool.Parse(configuration["Password:RequireLowercase"]);
+                    o.Password.RequireUppercase = bool.Parse(configuration["Password:RequireUppercase"]);
+                    o.Password.RequireNonAlphanumeric = bool.Parse(configuration["Password:RequireNonAlphanumeric"]);
+                }
+                catch (ArgumentNullException ex)
+                {
+                    throw new ServerErrorException($"Error during setting password configuration, argument: {ex.ParamName}");
+                }
+                catch (FormatException ex)
+                {
+                    throw new ServerErrorException($"Error during setting password configuration, argument: {ex.Message}");
+                }
+
+                o.User.RequireUniqueEmail = true;
+                o.SignIn.RequireConfirmedEmail = true;
+            })
+            .AddEntityFrameworkStores<DatabaseContext>()
+            .AddDefaultTokenProviders();
+
+            return services;
+        }
+
+        public static IServiceCollection SetupAuthentication(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(cfg =>
+            {
+                cfg.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidIssuer = configuration["tokens:issuer"],
+                    ValidAudience = configuration["tokens:audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["tokens:key"]))
+                };
+                cfg.Events = new JwtBearerEvents()
+                {
+                    OnMessageReceived = (context) =>
+                    {
+                        var accessToken = context.Request.Query["access_token"];
+
+                        var path = context.HttpContext.Request.Path;
+                        if (!string.IsNullOrEmpty(accessToken) &&
+                            (path.StartsWithSegments("/hub")))
+                        {
+                            context.Token = accessToken;
+                        }
+                        return Task.CompletedTask;
+                    }
+                };
+            });
+            return services;
+        }
+
+        public static IServiceCollection SetupHealthChecks(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddHealthChecks()
+                .AddCheck("SQL Database Check", () =>
+                {
+                    using (var conn = new SqlConnection(configuration.GetConnectionString("DefaultConnection")))
+                    {
+                        try
+                        {
+                            conn.Open();
+                            return HealthCheckResult.Healthy();
+                        }
+                        catch (SqlException)
+                        {
+                            return HealthCheckResult.Unhealthy();
+                        }
+                    }
+                });
+            return services;
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,135 +1,37 @@
 using PotatoServer.Database;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Newtonsoft.Json;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
-using System;
-using PotatoServer.Exceptions;
-using PotatoServer.Filters;
-using PotatoServer.Database.Models.Core;
-using Microsoft.Data.SqlClient;
-using Microsoft.Extensions.Diagnostics.HealthChecks;
 using PotatoServer.Hubs.TicTacToe;
 using PotatoServer.Hubs.Rooms;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
 
 namespace PotatoServer
 {
     public class Startup
     {
-        public IConfiguration Configuration { get; }
+        private readonly IConfiguration _configuration;
 
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            _configuration = configuration;
         }
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddCors(options =>
-            {
-                options.AddDefaultPolicy(builder =>
-                {
-                    builder
-                    .WithOrigins("http://localhost:4200")
-                    .AllowCredentials()
-                    .AllowAnyHeader()
-                    .AllowAnyMethod();
-                });
-            });
-
+            services.SetupCors();
             services.AddControllers()
-                .AddNewtonsoftJson(o => o.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore);
-
+                    .AddNewtonsoftJson(o => o.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore);
             services.AddLocalization(options => options.ResourcesPath = "Resources");
-            services.AddMvc(config =>
-            {
-                config.Filters.Add(new HandleExceptionAttribute());
-            })
-            .AddDataAnnotationsLocalization(options =>
-            {
-                options.DataAnnotationLocalizerProvider = (type, factory) => factory.Create(typeof(SharedResources));
-            });
-
-            services.AddDbContext<DatabaseContext>(o => o.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddIdentity<User, IdentityRole>(o =>
-            {
-                try
-                {
-                    o.Password.RequiredLength = int.Parse(Configuration["Password:RequiredLength"]);
-                    o.Password.RequireDigit = bool.Parse(Configuration["Password:RequireDigit"]);
-                    o.Password.RequireLowercase = bool.Parse(Configuration["Password:RequireLowercase"]);
-                    o.Password.RequireUppercase = bool.Parse(Configuration["Password:RequireUppercase"]);
-                    o.Password.RequireNonAlphanumeric = bool.Parse(Configuration["Password:RequireNonAlphanumeric"]);
-                }
-                catch (ArgumentNullException ex)
-                {
-                    throw new ServerErrorException($"Error during setting password configuration, argument: {ex.ParamName}");
-                }
-                catch (FormatException ex)
-                {
-                    throw new ServerErrorException($"Error during setting password configuration, argument: {ex.Message}");
-                }
-
-                o.User.RequireUniqueEmail = true;
-                o.SignIn.RequireConfirmedEmail = true;
-            })
-                .AddEntityFrameworkStores<DatabaseContext>()
-                .AddDefaultTokenProviders();
-
-            services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            }).AddJwtBearer(cfg =>
-             {
-                 cfg.TokenValidationParameters = new TokenValidationParameters()
-                 {
-                     ValidIssuer = Configuration["tokens:issuer"],
-                     ValidAudience = Configuration["tokens:audience"],
-                     IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["tokens:key"]))
-                 };
-                 cfg.Events = new JwtBearerEvents()
-                 {
-                     OnMessageReceived = (context) =>
-                     {
-                         var accessToken = context.Request.Query["access_token"];
-
-                         var path = context.HttpContext.Request.Path;
-                         if (!string.IsNullOrEmpty(accessToken) &&
-                             (path.StartsWithSegments("/hub")))
-                         {
-                             context.Token = accessToken;
-                         }
-                         return Task.CompletedTask;
-                     }
-                 };
-             });
-            
-            services.AddHealthChecks()
-                .AddCheck("SQL Database Check", () =>
-                {
-                    using (var conn = new SqlConnection(Configuration.GetConnectionString("DefaultConnection")))
-                    {
-                        try
-                        {
-                            conn.Open();
-                            return HealthCheckResult.Healthy();
-                        }
-                        catch (SqlException)
-                        {
-                            return HealthCheckResult.Unhealthy();
-                        }
-                    }
-                });
+            services.SetupMvc();
+            services.AddDbContext<DatabaseContext>(o => o.UseSqlServer(_configuration.GetConnectionString("DefaultConnection")));
+            services.SetupIdentity(_configuration);
+            services.SetupAuthentication(_configuration);
+            services.SetupHealthChecks(_configuration);
             services.AddSignalR(x => x.EnableDetailedErrors = true);
 
             services.AddSingleton<IRoomRepository, RoomsRepository>();
@@ -139,9 +41,8 @@ namespace PotatoServer
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
-            }
+
             app.UseCors();
             app.UseRouting();
 
