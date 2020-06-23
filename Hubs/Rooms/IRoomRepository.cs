585a872199b49154c513e5f29433d07606004478
Jonasz Makulak <j.makulak@gmail.com>
2020-06-23 18:47:37 +0200

    Some checks in RoomsHub

--- Hubs/Rooms/IRoomRepository.cs
+++ Hubs/Rooms/IRoomRepository.cs
@@ -1,9 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using PotatoServer.Database.Models.Core;
+using System.Collections.Generic;
 
 namespace PotatoServer.Hubs.Rooms
 {
     public interface IRoomRepository
     {
         public List<RoomGetVm> GetRooms(int skip, int take);
+        public int GetPlayerCount();
+        public void AddPlayer(string userName);
     }
 }
--- Hubs/Rooms/RoomsHub.cs
+++ Hubs/Rooms/RoomsHub.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
+using System;
 using System.Threading.Tasks;
 
 namespace PotatoServer.Hubs.Rooms
@@ -13,15 +14,21 @@ namespace PotatoServer.Hubs.Rooms
         {
             _roomRepository = roomRepository;
         }
+
+        public override async Task OnConnectedAsync()
+        {
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
+
+        [HubMethodName("getRooms")]
         public async Task GetRooms(int skip, int take)
         {
             var rooms = _roomRepository.GetRooms(skip, take);
             await Clients.All.SendAsync("onGetRooms", rooms);
-        }
-
-        public async Task PlayerEntered()
-        {
-           await Clients.All.SendAsync("OnPlayerEntered");
         }
     }
 }
